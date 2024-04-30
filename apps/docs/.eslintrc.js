a7cbc02c6f06f7f288e29c2d12a7069cd2032374
tyrannojung <tyranno2020@gmail.com>
2024-04-30 21:00:13 +0900

    Feat: Initialize monorepo and polyrepo structures

--- apps/docs/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  root: true,
-  extends: ["@nextoy/eslint-config"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
-};
---
+++ monorepo-initial/packages/eslint-config/next.js
@@ -0,0 +1,49 @@
+const { resolve } = require('node:path');
+
+const project = resolve(process.cwd(), 'tsconfig.json');
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: { tsconfigRootDir: __dirname, project, sourceType: 'module' },
+  extends: [
+    'eslint:recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'prettier',
+    require.resolve('@vercel/style-guide/eslint/next'),
+    'plugin:@typescript-eslint/recommended',
+    'eslint-config-turbo',
+  ],
+  globals: {
+    React: true,
+    JSX: true,
+  },
+  env: {
+    node: true,
+    browser: true,
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project,
+      },
+    },
+  },
+  ignorePatterns: [
+    // Ignore dotfiles
+    '.*.js',
+    'node_modules/',
+  ],
+  overrides: [{ files: ['*.js?(x)', '*.ts?(x)'] }],
+  plugins: ['@typescript-eslint'],
+  root: true,
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'react/jsx-filename-extension': ['error', { extensions: ['.tsx'] }],
+    'react/prop-types': 0, // prop types를 지정검사를 하는데, typescript가 검사해 주므로 0으로 한다.
+    'react/jsx-props-no-spreading': 0, // 0은 JSX 내의 props spreading을 허용
+    'react/require-default-props': 'off',
+  },
+};
